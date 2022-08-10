c74c3f2565da1d0a9c86686c65ab29a382853f2e
Taiseer Joudeh <tayseer_joudeh@hotmail.com>
2022-08-10 11:22:53 +0300

    Add Dapr SDK and invoke 1 single method

--- TasksTracker.WebPortal.Frontend.Ui/Pages/Tasks/Index.cshtml.cs
+++ TasksTracker.WebPortal.Frontend.Ui/Pages/Tasks/Index.cshtml.cs
@@ -22,11 +22,21 @@ namespace TasksTracker.WebPortal.Frontend.Ui.Pages.Tasks
         {
            
             TasksCreatedBy = Request.Cookies["TasksCreatedByCookie"];
+            
+            //Invoke via internal URL (Not Dapr)
+            //var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
+            //TasksList = await httpClient.GetFromJsonAsync<List<TaskModel>>($"api/tasks?createdBy={TasksCreatedBy}");
 
-            var httpClient = _httpClientFactory.CreateClient("BackEndApiExternal");
 
-            var result = await httpClient.GetFromJsonAsync<List<TaskModel>>($"api/tasks?createdBy={TasksCreatedBy}");
+            // Invoke via Dapr SideCar URL
+            //var port = 3500;//Environment.GetEnvironmentVariable("DAPR_HTTP_PORT");
+            //HttpClient client = new HttpClient();
+            //var result = await client.GetFromJsonAsync<List<TaskModel>>($"http://localhost:{port}/v1.0/invoke/tasksmanager-backend-api/method/api/tasks?createdBy={TasksCreatedBy}");
+            //TasksList = result;
 
+            // Invoke via DaprSDK
+            var daprCLient = new Dapr.Client.DaprClientBuilder().Build();
+            var result = await daprCLient.InvokeMethodAsync<List<TaskModel>>(HttpMethod.Get, "tasksmanager-backend-api", $"api/tasks?createdBy={TasksCreatedBy}");
             TasksList = result;
 
         }
